4de2bb611ddd2b11b6a8bee004f6155032453498
ashcarlsen <ashtoncarlsen@yahoo.com>
2024-02-26 17:12:24 -0700

    Changed GPS to use pin PC5 in order to free up pin for keypad

--- Caddy/GPS.c
+++ Caddy/GPS.c
@@ -5,13 +5,13 @@
 
 /*============================
 *	Pins:
-*	B7: USART1 Rx
+*	C5: USART3 Rx
 *============================*/
 
 void GPS_Read_NMEA(char *buffer, uint32_t size)
 	{
-	USART1->CR1 |= USART_CR1_UE; //enable USART
-	while((USART1->ISR & USART_ISR_REACK) == 0); //recieve enable flag
+	USART3->CR1 |= USART_CR1_UE; //enable USART
+	while((USART3->ISR & USART_ISR_REACK) == 0); //recieve enable flag
 	unsigned int i = 0;
 	char done = 0;
 	char valid = 0;
@@ -21,8 +21,8 @@ void GPS_Read_NMEA(char *buffer, uint32_t size)
 	}
 	for(int i = 0; i < size; i++)	//Read into the entire buffer
 	{
-		while(!(USART1->ISR & USART_ISR_RXNE));
-		buffer[i] = USART1->RDR;
+		while(!(USART3->ISR & USART_ISR_RXNE));
+		buffer[i] = USART3->RDR;
 		if(buffer[i] == '$')
 		{
 			valid = 1;
@@ -44,30 +44,30 @@ void GPS_Read_NMEA(char *buffer, uint32_t size)
 			}
 		}
 	}
-	USART1->CR1 &= ~USART_CR1_UE; //disable USART
+	USART3->CR1 &= ~USART_CR1_UE; //disable USART
 }
 
 void GPS_Init(void){
 	GPS_GPIO_Init();
-	RCC->APB2ENR |= RCC_APB2ENR_USART1EN; //Enable UART Clock
-	USART1->CR1 &= ~USART_CR1_UE; //disable USART
-	USART1->CR1 &= ~(USART_CR1_M); //set to 8-bit word, 1 start, n stop bits
-	USART1->CR2 &= ~(USART_CR2_STOP); //set to 1 stop bit
-	USART1->CR1 &= ~(USART_CR1_PCE); //disable parity control bits
-	USART1->CR1 &= ~(USART_CR1_OVER8); //clear over8 bit to 16 oversampling
-	USART1->BRR = 0x682; //9600 baud
-	USART1->CR1 |= (USART_CR1_RE); //enable transmit and receive
-	USART1->CR1 |= USART_CR1_UE; //enable USART
-	while((USART1->ISR & USART_ISR_REACK) == 0); //recieve enable flag
+	RCC->APB1ENR1 |= RCC_APB1ENR1_USART3EN; //Enable UART Clock
+	USART3->CR1 &= ~USART_CR1_UE; //disable USART
+	USART3->CR1 &= ~(USART_CR1_M); //set to 8-bit word, 1 start, n stop bits
+	USART3->CR2 &= ~(USART_CR2_STOP); //set to 1 stop bit
+	USART3->CR1 &= ~(USART_CR1_PCE); //disable parity control bits
+	USART3->CR1 &= ~(USART_CR1_OVER8); //clear over8 bit to 16 oversampling
+	USART3->BRR = 0x682; //9600 baud
+	USART3->CR1 |= (USART_CR1_RE); //enable transmit and receive
+	USART3->CR1 |= USART_CR1_UE; //enable USART
+	while((USART3->ISR & USART_ISR_REACK) == 0); //recieve enable flag
 }
 
 void GPS_GPIO_Init(void){
-	RCC->AHB2ENR |= RCC_AHB2ENR_GPIOBEN;	//enable clock on GPIOA
-	GPIOB->MODER &= ~GPIO_MODER_MODE7;
-	GPIOB->MODER |= GPIO_MODER_MODE7_1; //sets pin B7 to analog function
-	GPIOB->AFR[0] |= 0x77 << (4*6);		// Sets pinB6-7 as analog if we want TX too
-	GPIOB->PUPDR &= ~(GPIO_PUPDR_PUPD7);
-	GPIOB->PUPDR |= (GPIO_PUPDR_PUPD7_0); //Set as Pull-up
-	GPIOB->OSPEEDR |= (GPIO_OSPEEDR_OSPEED7); //Set as high speed
-	GPIOB->OTYPER &= ~(GPIO_OTYPER_OT7);	// Push-pull output type
+	RCC->AHB2ENR |= RCC_AHB2ENR_GPIOCEN;	//enable clock on GPIOA
+	GPIOC->MODER &= ~GPIO_MODER_MODE5;
+	GPIOC->MODER |= GPIO_MODER_MODE5_1; //sets pin C5 to analog function
+	GPIOC->AFR[0] |= 0x77 << (4*4);		// Sets pinC4-5 as analog if we want TX too
+	GPIOC->PUPDR &= ~(GPIO_PUPDR_PUPD5);
+	GPIOC->PUPDR |= (GPIO_PUPDR_PUPD5_0); //Set as Pull-up
+	GPIOC->OSPEEDR |= (GPIO_OSPEEDR_OSPEED5); //Set as high speed
+	GPIOC->OTYPER &= ~(GPIO_OTYPER_OT5);	// Push-pull output type
 }
