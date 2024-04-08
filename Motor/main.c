a5f93d01a80d4d140f754cbef9fc3d1dc9167028
ashcarlsen <ashtoncarlsen@yahoo.com>
2024-04-08 12:08:49 -0600

    Accurate spinning motor control

--- Motor/main.c
+++ Motor/main.c
@@ -1,8 +1,13 @@
 #include "stm32l476xx.h"
 #include "Motor.h"
 
+#define POS_COUNT 576
+#define HALF_COUNT 280
+#define TEN_COUNT 56
+
 static int pos;
-static int rotations;
+static float rotations;
+static uint16_t club_pos;
 
 void EXTI_Init(void) {
 	RCC->APB2ENR |= RCC_APB2ENR_SYSCFGEN;
@@ -25,14 +30,38 @@ void EXTI1_IRQHandler(void){
 		{
 			pos = pos - 1;
 		}
-		if(pos >= 576)
+		if(pos >= TEN_COUNT)
 		{
-			rotations += 1;
+			rotations += 0.1;
+			if(rotations >= 0.4)
+			{
+				if(club_pos == 12)
+				{
+					club_pos = 1;
+				}
+				else
+				{
+					club_pos += 1;
+				}
+				rotations = 0;
+			}
 			pos = 0;
 		}
-		else if(pos <= -576)
+		else if(pos <= -TEN_COUNT)
 		{
-			rotations -= 1;
+			rotations -= 0.1;
+			if(rotations <= -0.4)
+			{
+				if(club_pos == 1)
+				{
+					club_pos = 12;
+				}
+				else
+				{
+					club_pos -= 1;
+				}
+				rotations = 0;
+			}
 			pos = 0;
 		}
 		EXTI->PR1 |= EXTI_PR1_PIF1;
@@ -46,6 +75,7 @@ int main()
 {
 	pos = 0;
 	rotations = 0;
+	club_pos = 1;
 	EXTI_Init();
 	prvMotorGPIO_Setup();
 	// Set GPIOC pin 13 to input for Button
